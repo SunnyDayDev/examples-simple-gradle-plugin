569d770afda487a4c49bb8c6d39de96ad93ed980
Александр Цикин <sunnyday.development@gmail.com>
2017-10-03 21:47:56 +0300

    Kotlin + Base plugin

--- autoincrementor/src/main/java/me/sunnydaydev/autoincrementor/AutoIncrementorPlugin.java
+++
@@ -1,4 +0,0 @@
-package me.sunnydaydev.autoincrementor;
-
-public class AutoIncrementorPlugin {
-}
---
+++ autoincrementor/src/main/kotlin/me/sunnydaydev/autoincrementor/AutoIncrementorPlugin.kt
@@ -0,0 +1,12 @@
+package me.sunnydaydev.autoincrementor
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AutoIncrementorPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+
+    }
+
+}
